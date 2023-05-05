d6314b0691f4892ec087378326c4ea182c143753
Hongyu Wang <hwang135@sheffield.ac.uk>
2023-05-05 16:57:48 +0100

    The DB

--- teamAssignment/app.js
+++ teamAssignment/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var birdSightRouter = require("./routes/sightings");
 
 var app = express();
 
@@ -14,13 +15,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/sights", birdSightRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- teamAssignment/routes/index.js
+++ teamAssignment/routes/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
+const {BirdSightModel} = require("../databases/bird_sights");
 var router = express.Router();
-
+var createError = require('http-errors');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+
+
+  res.render('index');
 });
 
-module.exports = router;
 
 router.get('/add_sight', function(req, res, next) {
   res.render('add_bird_sight', {title: 'Add  a Bird Sight'});
 })
+
+router.get('/bird_sight/:id', async function(req, res) {
+  const {id} = req.params;
+  const bird_sight = await BirdSightModel.findById(id);
+  if (!bird_sight) {
+    return createError(404);
+  }
+  res.render("bird_sight", {
+    bird_sight
+  })
+});
+
+module.exports = router;
