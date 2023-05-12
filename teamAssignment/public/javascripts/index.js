e4e31c1d19789555e62e853d8b718206ded3d5cf
jlaybourn <83641545+jlaybourn@users.noreply.github.com>
2023-05-12 03:32:21 +0100

    Offline
    Service Worker now working. The user can now access the web app offline. The service worker will update when back online with any changes.

--- teamAssignment/public/javascripts/index.js
+++ teamAssignment/public/javascripts/index.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+  // Register the service worker
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
 
 let identified = false;
@@ -34,6 +46,13 @@ sortDistanceButton.addEventListener('click', () => {
   renderSights();
   renderSortButtons();
 });
+let allSights = [];
+fetch('/sights')
+    .then(res => res.json())
+    .then(data => {
+      allSights = data;
+      renderSights();
+    })
 
 function renderSortButtons() {
   // render datetime seen sort button
@@ -118,10 +137,4 @@ function renderSights() {
 
 }
 
-let allSights = [];
-fetch('/sights')
-  .then(res => res.json())
-  .then(data => {
-    allSights = data;
-    renderSights();
-  })
+
